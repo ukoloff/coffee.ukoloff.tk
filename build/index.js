24cab68aa28b5dfcecbf1fe8d470f5f794bc1723
Stas Ukolov <ukoloff@gmail.com>
2014-10-10 10:31:22 +0600

    Build UglifyJS

--- build/index.js
+++ build/index.js
@@ -1,1 +1,13 @@
-console.log('Hi!')
+var
+  fs = require('fs'),
+  path = require('path'),
+  chp = require('child_process')
+
+
+console.log('Building UglifyJS...')
+var u=chp.spawn(process.argv[0],
+  ['uglifyjs', '--self'], 
+  {cwd: path.join(__dirname, 'node_modules/uglify-js/bin')})
+.on('error', function(e){console.log('Error:', e.message)})
+
+u.stdout.pipe(fs.createWriteStream(path.join(__dirname, '../js/uglify.js')))
