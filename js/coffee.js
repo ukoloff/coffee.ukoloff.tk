689fd43c98093270edceafd313c08f0229fc2c2c
Stas Ukolov <ukoloff@gmail.com>
2015-03-28 21:06:42 +0500

    Init editors

--- js/coffee.js
+++ js/coffee.js
@@ -1,11 +1,37 @@
 !function(){
 
 var
-  versions = []
+  versions = [],
+  editors = {}
+
+setTimeout(boot)
 
 this.define = function(vs)
 {
   versions = vs
+}
+
+function boot()
+{
+  initEditors()
+}
+
+function initEditors()
+{
+  var z = document.getElementsByTagName('pre')
+  for(var i = z.length-1; i>=0; i--)
+  {
+    var id = z[i].id
+    var x = editors[id] = ace.edit(id)
+    x.setTheme("ace/theme/github")
+    x.getSession().setMode("ace/mode/"+id)
+    x.getSession().setUseWorker(false)
+    x.on('focus', hidePopup)
+  }
+}
+
+function hidePopup()
+{
 }
 
 }()
