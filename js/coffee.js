d8ea3819110c23f60dbe31c9e151f1d1fc0d8394
Stas Ukolov <ukoloff@gmail.com>
2015-03-28 21:59:44 +0500

    Fill select

--- js/coffee.js
+++ js/coffee.js
@@ -41,12 +41,26 @@ function initPopup()
     popup.style.display = 'block'
   }
   popup.getElementsByTagName('a')[0].onclick = hidePopup
+  z=popup.getElementsByTagName('select')[0]
+  var s=''
+  for(var i = 0; i<versions.length; i++)
+  {
+    var x = document.createElement("option")
+    x.text=versions[i]
+    z.add(x)
+  }
+  z.onchange=select
 }
 
 function hidePopup()
 {
   popup.style.display=''
   return false
+}
+
+function select()
+{
+
 }
 
 }()
