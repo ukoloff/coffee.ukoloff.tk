3716efd4df935229793c396ac4261b99a9509ec4
Stas Ukolov <ukoloff@gmail.com>
2019-04-12 19:51:01 +0500

    Fetch versions

---
+++ build/fetch/index.js
@@ -0,0 +1,47 @@
+const fetch = require('node-fetch')
+
+const repos = ['jashkenas/coffeescript', 'gkz/LiveScript']
+
+Promise.all(repos.map(listTags))
+.then(buildList)
+.catch(Error)
+
+function listTags(repo) {
+  return fetch(`https://api.github.com/repos/${repo}/git/refs/tags`)
+  .then(x => x.json())
+}
+
+function Error(error) {
+  console.log(`# ERROR: ${error}!`)
+  process.exit(1)
+}
+
+function buildList(arr) {
+  var res = {}
+  zip(repos.map(langName), arr.map(repoTags)).forEach(([k, v]) => res[k] = v)
+  console.log(JSON.stringify(res))
+}
+
+function repoTags(tags) {
+  return tags
+  .map(x => x.ref.replace(/[^]*\/v?/i, ''))
+  .filter(x => /^\d+([.]\d+)*$/.test(x))
+  .reverse()
+}
+
+function langName(repo) {
+  return repo.replace(/[^]*\//, '').toLowerCase()
+}
+
+function zip(...arrs) {
+  return arrs[0].map(zip)
+
+  function zip(_, row) {
+    return arrs.map(item)
+
+    function item(_, column) {
+      return arrs[column][row]
+    }
+  }
+}
+
